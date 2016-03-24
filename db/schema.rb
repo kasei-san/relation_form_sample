1968e03479093076da374a3493a29c67290efd6c
kasei_san <kasei.san@gmail.com>
2016-03-24 21:05:26 +0900

    $rails g scaffold photo

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160324120026) do
+ActiveRecord::Schema.define(version: 20160324120317) do
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
     t.integer  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "item_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+1.upto(10) do |i|
+  item = Item.create(name: "item_#{i}", price: i*100)
+  1.upto(5) do |j|
+    item.photos << Photo.create(title: "#{i}_#{j}")
+  end
+end
